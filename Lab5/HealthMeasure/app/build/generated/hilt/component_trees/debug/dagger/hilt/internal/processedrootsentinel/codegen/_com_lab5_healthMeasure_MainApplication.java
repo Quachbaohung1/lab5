60bcf2ab33249445b76820be9288e13ed0779733
Quachbaohung1 <Baohung9715@gmail.com>
2022-06-07 23:37:00 +0700

    finish

---
+++ Lab5/HealthMeasure/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_lab5_healthMeasure_MainApplication.java
@@ -0,0 +1,9 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+@ProcessedRootSentinel(
+    roots = "com.lab5.healthMeasure.MainApplication"
+)
+public final class _com_lab5_healthMeasure_MainApplication {
+}
